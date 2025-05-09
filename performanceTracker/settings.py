8ebf52ebe52d7ee036e36fe65143291d75628d09
Andrew <dreandrew11091@gmail.com>
2025-05-09 08:13:03 +0300

    renderhostingprep

--- performanceTracker/settings.py
+++ performanceTracker/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x_wlh)(%s_=*0rz#^=s9b1id9=j4iwchj=2j%4-!g=c&w#=@o$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['base.onrender.com']
 
 
 # Application definition
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
